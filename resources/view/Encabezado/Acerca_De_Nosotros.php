833a638d2f4179dd8c6615420055c76ce23acab8
RobertoVega <vega.villanueva.roberto@gmail.com>
2022-05-21 13:38:58 -0600

    Acerca de nosotros

---
+++ resources/view/Encabezado/Acerca_De_Nosotros.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de DLR</title>
+</head>
+<body>
+    <H1>ACERCA DE NOSOTROS</H1>
+    <H2>DLR</H2>
+    
+    <P>
+        DLR (Dulcería los Roberts) tiene este nombre ya que los socios que crearon este sitio <BR>
+        comparten el nombre Roberto, ambos socios estuvieron de acuerdo con este nombre.</BR>
+    </P>
+    
+    <H2>Socios</H2>
+
+    <p>
+        Roberto Ismael Acosta López <BR>
+        Roberto Vega Villanueva
+    </p>
+
+    <H2>Contáctenos</H2>
+        <Table>
+            <tbody>
+                <tr>
+                    <td>
+                        Correo
+                    </td>
+                    <td>
+                        L18111934@itcj.edu.mx <BR>
+                        L18111958@itcj.edu.mx
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Telefono
+                    </td>
+                    <td>
+                        656-468-4688
+                    </td>
+                </tr>
+            </tbody>
+        </Table>
+
+    <H2>Fundación de la página</H2>
+    <p>
+        20 de mayo de 2022
+    </p>
+    <H2>Fecha de actualización</H2>
+    <p>
+        21 de mayo de 2022
+    </p>
+</body>
+</html>
