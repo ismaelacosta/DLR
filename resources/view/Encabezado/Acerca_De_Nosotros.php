f1653915557dcffb111b13a19f1ee404bd025f6f
RobertoVega <vega.villanueva.roberto@gmail.com>
2022-05-21 13:43:20 -0600

    Diseño de página Acerca de Nosotros

--- resources/view/Encabezado/Acerca_De_Nosotros.php
+++ resources/view/Encabezado/Acerca_De_Nosotros.php
@@ -1,29 +1,110 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acerca de DLR</title>
-</head>
-<body>
-    <H1>ACERCA DE NOSOTROS</H1>
-    <H2>DLR</H2>
-    
-    <P>
-        DLR (Dulcería los Roberts) tiene este nombre ya que los socios que crearon este sitio <BR>
-        comparten el nombre Roberto, ambos socios estuvieron de acuerdo con este nombre.</BR>
-    </P>
-    
-    <H2>Socios</H2>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <p>
-        Roberto Ismael Acosta López <BR>
-        Roberto Vega Villanueva
-    </p>
+    <title>Acerca de nosotros (DLR)</title>
 
-    <H2>Contáctenos</H2>
-        <Table>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/features/">
+
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="features.css" rel="stylesheet">
+  </head>
+  <body>
+    
+
+<main>
+  <h1 class="visually-hidden">Features examples</h1>
+
+  <div class="container px-4 py-5" id="featured-3">
+    <h2 class="pb-2 border-bottom">ACERCA DE NOSOTROS</h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-primary bg-gradient text-white fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#collection"/></svg>
+        </div>
+        <h2>DLR</h2>
+        <p>
+          DLR (Dulcería los Roberts) tiene este nombre ya que los socios que crearon este sitio 
+          comparten el nombre Roberto, ambos socios estuvieron de acuerdo con este nombre.
+        </p>
+
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-primary bg-gradient text-white fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"/></svg>
+        </div>
+        <h2>Socios</h2>
+        <p>
+          - Roberto Ismael Acosta López <BR>
+          - Roberto Vega Villanueva
+        </p>
+
+      </div>
+      <div class="feature col">
+        <div class="feature-icon d-inline-flex align-items-center justify-content-center bg-primary bg-gradient text-white fs-2 mb-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"/></svg>
+        </div>
+        <h2>Contáctenos</h2>
+        <p>
+          <Table> 
             <tbody>
                 <tr>
                     <td>
@@ -44,14 +125,49 @@
                 </tr>
             </tbody>
         </Table>
+        </p>
+      </div>
+    </div>
+  </div>
 
-    <H2>Fundación de la página</H2>
-    <p>
-        20 de mayo de 2022
-    </p>
-    <H2>Fecha de actualización</H2>
-    <p>
-        21 de mayo de 2022
-    </p>
-</body>
+  <div class="b-example-divider"></div>
+
+  <div class="container px-4 py-5" id="hanging-icons">
+    <h2 class="pb-2 border-bottom">ACERCA DE LA PÁGINA</h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="col d-flex align-items-start">
+        <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#toggles2"/></svg>
+        </div>
+        <div>
+          <h2>Fundación de la página</h2>
+          <p>
+            20 de mayo de 2022
+          </p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#cpu-fill"/></svg>
+        </div>
+        <div>
+          <h2>Fecha de actualización</h2>
+          <p>21 de mayo de 2022</p>
+
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <div class="icon-square bg-light text-dark d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+          <svg class="bi" width="1em" height="1em"><use xlink:href="#tools"/></svg>
+        </div>
+        <div>
+          <h2>Solo en Juárez</h2>
+          <p>Por el momento DLR solo hace entregas de mercancía en el área de Cd. Juárez</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+  </body>
 </html>
